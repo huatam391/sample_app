40367687b4d6506f7e41d4038f15ff749d816155
Hua Thanh Tam <huatam391@gmail.com>
2019-03-21 14:43:25 +0700

    Chapter 4: Add full a title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Return the full title on a per-page basis.
+  def full_title page_title
+    base_title = I18n.t "helper.application.base_title"
+    if page_title.blank?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
