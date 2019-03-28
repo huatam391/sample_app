674fde4f7a7cd6982379a2737fcfe06a49154a00
Hua Thanh Tam <huatam391@gmail.com>
2019-03-28 09:13:22 +0700

    Chapter 7: Finished user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,33 @@
 class UsersController < ApplicationController
-  def new; end
+  before_action :load_user, only: [:show]
+  def show; end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params # Not the final implementation!
+    if @user.save
+      flash[:success] = t("controllers.users_controller.flash-message")
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def load_user
+    @user = User.find_by(id: params[:id])
+      return unless @user.nil?
+      flash[:danger] = t("controllers.users_controller.usernotEX")
+      redirect_to root_path
+    end
+  end
+
+  def user_params
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation
+  end
 end
