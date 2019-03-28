1ae5d090d9dcf2c60e3e330fa3b509c747dff846
Hua Thanh Tam <huatam391@gmail.com>
2019-03-28 09:13:22 +0700

    Chapter 10: Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,33 +1,77 @@
 class UsersController < ApplicationController
-  before_action :load_user, only: [:show]
+  before_action :logged_in_user, except: %i(new create show)
+  before_action :load_user, except: %i(index new create)
+  before_action :correct_user, only: %i(edit update)
+  before_action :is_admin?, only: :destroy
+
   def show; end
 
   def new
     @user = User.new
   end
 
+  def index
+    @users = User.paginate page: params[:page], per_page: Settings.perpage
+  end
+
   def create
     @user = User.new user_params # Not the final implementation!
     if @user.save
-      flash[:success] = t ".flash-message"
       log_in @user
+      flash[:success] = t ".welcome"
       redirect_to @user
     else
       render :new
     end
   end
 
-  private
+  def edit; end
 
-  def load_user
-    @user = User.find_by id: params[:id]
-    return if @user
-    flash[:danger] = t ".usernotEX"
-    redirect_to root_path
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t ".update"
+      redirect_to @user
+    else
+      render :edit
+    end
   end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t ".delete"
+    else
+      flash[:danger] = t ".danger_delete"
+    end
+    redirect_to users_path
+  end
+
+  private
 
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation
+  end
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    return if @user
+    flash[:danger] = t ".not_found"
+    redirect_to root_path
+  end
+
+  def logged_in_user
+    return if logged_in?
+    store_location
+    flash[:danger] = t ".pl_login"
+    redirect_to login_path
+  end
+
+  # Confirms the correct user.
+  def correct_user
+    redirect_to root_path unless current_user? @user
+  end
+
+  def is_admin?
+    redirect_to root_path unless current_user.admin?
   end
 end
