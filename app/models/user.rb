5687e1839586abadaa0b03fefa6905675a02ce46
Hua Thanh Tam <huatam391@gmail.com>
2019-03-28 09:13:22 +0700

    Chapter 13: Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
@@ -54,8 +55,6 @@ class User < ApplicationRecord
   # Sets the password reset attributes.
   def create_reset_digest
     self.reset_token = User.new_token
-    # update_attribute :reset_digest,  User.digest(reset_token)
-    # update_attribute :reset_sent_at, Time.zone.now
     update_attributes reset_digest: User.digest(reset_token),
       reset_sent_at: Time.zone.now
   end
@@ -67,6 +66,10 @@ class User < ApplicationRecord
 
   def password_reset_expired?
     reset_sent_at < Settings.two.hours.ago
+  end
+
+  def feed
+    Micropost.where_id(id).order_desc
   end
 
   private
